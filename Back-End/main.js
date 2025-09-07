d0286bc6ce2cdb8a7efc80a72a0ba7cc425c97ed
Mahammud Hassan Limon <156072234+Limon-Hassan@users.noreply.github.com>
2025-09-07 23:30:03 +0600

    category done

--- Back-End/main.js
+++ Back-End/main.js
@@ -11,8 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.use('/uploads', express.static('uploads'));
-app.use('/productPhoto', express.static('productPhoto'));
+app.use(express.static('uploads'));
+app.use(express.static('productPhoto'));
 app.use(router);
 app.use(ErrorCheck);
 dbConnection();
